3012d2f8c9f022f387c3ebca6ee26577447192f0
Hung150 <55206412+Hung150@users.noreply.github.com>
2026-01-13 16:17:08 +0300

    Create index.ts

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,11 @@
+// Export types
+export * from './types';
+
+// Export store
+export * from './store';
+
+// Export utils
+export * from './utils/helpers';
+
+// Export hooks
+export { useAppDispatch, useAppSelector } from './hooks';
